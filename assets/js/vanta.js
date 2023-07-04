4434bf9ea82908d58ddf1637181a40ee1f6b6d39
Max Lareau <max@maxlareau.com>
2023-07-04 19:44:52 -0400

    Establish base site
    - Site setup and configuration
    - Establish color scheme

---
+++ assets/js/vanta.js
@@ -0,0 +1,11 @@
+VANTA.HALO({
+    el: "#vanta",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    amplitudeFactor: 2.70,
+    xOffset: 0.05,
+    yOffset: -0.05
+})
