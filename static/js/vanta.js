76cc7e6d44916f556709974c188cae8d046f774d
Max Lareau <max@maxlareau.com>
2023-07-08 00:42:28 -0400

    Update vanta config

--- static/js/vanta.js
+++ static/js/vanta.js
@@ -1,15 +1,19 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
-    console.log("Vanta background init...");
-    VANTA.HALO({
-        el: "#vanta-bg",
-        mouseControls: false,
-        touchControls: false,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        amplitudeFactor: 0.5,
-        xOffset: 0,
-        yOffset: -0
-    })
+    const { pathname } = window.location
+    if (pathname === '/') {
+        console.log("Vanta background init...");
+        VANTA.HALO({
+            el: "#vanta-bg",
+            mouseControls: false,
+            touchControls: false,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            amplitudeFactor: 0,
+            xOffset: 0,
+            yOffset: 0.05,
+            size: .5
+        })
+    }
 });
+window.removeEventListener('DOMContentLoaded')
